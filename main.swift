1fb97d227727f0d1925f5559e8b8f8d974fe6b34
Artur Arystanbekov <arturarystanbekov@MacBook-Air-Artur.local>
2026-09-18 23:58:16 +0500

    Initial commit

---
+++ main.swift
@@ -0,0 +1,23 @@
+import Foundation
+let firstname="Artur"
+let Lastname="Arystanbekov"
+var age=20
+var birthyear=2005
+let isstudent=true
+let height=1.75
+var currentyear=2026
+var agenow=currentyear-birthyear
+let hobby="wayching movies"
+var numofhobbie=3
+var favnum=7
+let isHobbyCreative=false
+let futureGoals = "become a professional iOS developer"
+let lifeStory = """
+My name is \(firstname) \(Lastname).
+I am \(agenow) years old, born in \(birthyear).
+My height is \(height) meters.
+I enjoy \(hobby), which is a creative hobby: \(isHobbyCreative).
+I have \(numofhobbie) hobbies in total, and my favorite number is \(favnum).
+In the future, I want to \(futureGoals).
+"""
+print(lifeStory)
