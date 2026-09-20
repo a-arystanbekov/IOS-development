a3d902bd19435bf709a4fb478a2a145b168a064f
Artur Arystanbekov <arturarystanbekov@MacBook-Air-Artur.local>
2026-09-20 05:36:56 +0500

    Separate Lab1 and Lab2

--- main.swift
+++ main.swift
@@ -1,36 +1,23 @@
 import Foundation
-//easy
-var fruits = ["Apple", "Banana", "Orange", "Pineple", "Grape"]
-print(fruits[2])
-var Numbers: Set = [1, 2, 3, 4]
-Numbers.insert(7)
-print(Numbers)
-var langDict: [String : Int] = ["Python": 1995, "C++": 1989, "Java": 1988]
-print(langDict["Python"]!)
-var colours = ["Red", "Silver", "Black", "Purple"]
-colours[1] = "Sherry"
-print(colours)
-//med
-let firstSet: Set = [1, 2, 3, 4]
-let secondSet: Set = [3, 4, 5 , 6]
-let res = firstSet.intersection(secondSet)
-print(res)
-var studentsGrade: [String:Int] = ["Bob":87, "Draven": 88, "Riley":56]
-studentsGrade["Draven"] = 76
-print(studentsGrade)
-let first = ["banana", "apple"]
-let second = ["cherry", "date"]
-let third = first + second
-print(third)
-//hard
-var countries: [String:Int] = ["Kzazkhstan" : 20, "USA" : 120, "Russia" : 100]
-countries["Turkey"] = 88
-print(countries)
-var fset: Set<String> = ["Cat", "Dog"]
-var sset: Set<String> = ["Dog", "Mouse"]
-let unionSet = fset.union(sset)
-fset = unionSet
-fset.subtract(sset)
-print(fset)
-let grades: [String:[Int]] = ["Artur":[78, 67, 89], "Dragos":[43, 54, 23], "Vasile": [54, 78, 89]]
-print(grades["Artur"]![1])
+let firstname="Artur"
+let Lastname="Arystanbekov"
+var age=20
+var birthyear=2005
+let isstudent=true
+let height=1.75
+var currentyear=2026
+var agenow=currentyear-birthyear
+let hobby="wayching movies"
+var numofhobbie=3
+var favnum=7
+let isHobbyCreative=false
+let futureGoals = "become a professional iOS developer"
+let lifeStory = """
+My name is \(firstname) \(Lastname).
+I am \(agenow) years old, born in \(birthyear).
+My height is \(height) meters.
+I enjoy \(hobby), which is a creative hobby: \(isHobbyCreative).
+I have \(numofhobbie) hobbies in total, and my favorite number is \(favnum).
+In the future, I want to \(futureGoals).
+"""
+print(lifeStory)
